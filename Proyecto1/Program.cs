3af8d3737f8776a8931861e6101676ad0ca4ebd8
WalterJuarez <68527309+WalterJuarez@users.noreply.github.com>
2021-03-10 15:46:06 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Proyecto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string op;
+            string codSys = "C:/";
+            
+            Console.Write(codSys);
+            op = Console.ReadLine();  
+            Opciones();
+            
+        }
+        public static void Opciones()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("search:      Buscar un repositorio");
+            Console.WriteLine("create:      Crea un repositorio");
+            Console.WriteLine("binnacle:    Bitacora de registros del repositorio");
+            Console.WriteLine("delete:      Borra un registro");
+        }
+    }
+}
